322e8a8d37cd060e23e6f0effa300c4fdf7a803f
Austin <higgins.austin@gmail.com>
2017-10-31 10:04:40 -0500

    Added comments

--- query.py
+++ query.py
@@ -1,8 +1,12 @@
 import sqlite3
 import time
 
+# Ensure the databas name is correct and in the correct folder 
+
 conn = sqlite3.connect("feedback.db")
 cursor = conn.cursor()
+
+# Only prints contents of database to console
 
 for row in cursor.execute("SELECT rowid, * FROM feedback ORDER BY device"):
 	print row
